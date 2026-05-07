c2b7bc8d437da8e7c61564ac24d13270d5202aa4
frangarciaharo <frangarcia.nuria@gmail.com>
2026-05-07 20:31:31 +0200

    Añadido el asignar profesor

--- src/Domain/Subject/Subject.php
+++ src/Domain/Subject/Subject.php
@@ -4,26 +4,36 @@ namespace App\Domain\Subject;
 
 use Doctrine\ORM\Mapping as Orm;
 use App\Domain\Course\Course;
-
+use App\Domain\Teacher\Teacher;
 
 #[Orm\Entity]
 #[Orm\Table(name: 'subjects')]
-class Subject{
+class Subject
+{
     #[Orm\Id, Orm\Column(type: 'string', length: 5)]
     private string $code;
+
     #[Orm\Column(type: 'string')]
     private string $namesubject;
-    #[Orm\Column(type: 'string', nullable: true)]
-    private ?string $teachercode = null;
+
     #[Orm\Column(type: 'integer')]
     private int $duration;
-    #[ORM\ManyToOne(targetEntity: Course::class, inversedBy: 'subjects')]
-    #[ORM\JoinColumn(
+
+    #[Orm\ManyToOne(targetEntity: Course::class, inversedBy: 'subjects')]
+    #[Orm\JoinColumn(
         name: 'code_course',
         referencedColumnName: 'code_course',
         onDelete: 'CASCADE'
     )]
     private ?Course $course = null;
+
+    #[Orm\ManyToOne(targetEntity: Teacher::class)]
+    #[Orm\JoinColumn(
+        name: 'code_teacher',
+        referencedColumnName: 'code',
+        nullable: true
+    )]
+    private ?Teacher $teacher = null;
 
     public function __construct(string $code, string $namesubject, int $duration)
     {
@@ -32,8 +42,7 @@ class Subject{
         $this->setDuration($duration);
     }
 
-    
-    public function getCode()
+    public function getCode(): string
     {
         return $this->code;
     }
@@ -41,52 +50,74 @@ class Subject{
     public function setCode(string $code): void
     {
         $this->code = $code;
-    } 
+    }
 
-    public function getNamesubject()
+    public function getNamesubject(): string
     {
         return $this->namesubject;
     }
 
-    public function setNamesubject(String $namesubject): void
-    {   
-          if (empty(trim($namesubject))) {
+    public function setNamesubject(string $namesubject): void
+    {
+        if (empty(trim($namesubject))) {
             throw new \InvalidArgumentException("Subject cannot be empty");
         }
-        if(strlen($namesubject) < 4){
+
+        if (strlen($namesubject) < 4) {
             throw new \InvalidArgumentException("Subject must be at least 4 characters long");
         }
+
         $this->namesubject = $namesubject;
     }
-    public function getDuration()
+
+    public function getDuration(): int
     {
         return $this->duration;
     }
+
     public function setDuration(int $duration): void
     {
-        if($duration < 2){
-            throw new \InvalidArgumentException("Duration min 2 valors: ex 10");
+        if ($duration < 2) {
+            throw new \InvalidArgumentException("Duration min 2");
         }
+
         $this->duration = $duration;
     }
+
     public function setCourse(?Course $course): void
     {
         $this->course = $course;
     }
+
+    public function getCourse(): ?Course
+    {
+        return $this->course;
+    }
+
     public function getCourseCode(): ?string
     {
-        return $this->course ? $this->course->getCodeCourse() : null;
+        return $this->course?->getCodeCourse();
     }
 
-    public function getTeachercode(){
-        return $this->teachercode ? $this->teachercode: null;
+
+    public function assignTeacher(?Teacher $teacher): void
+    {
+        $this->teacher = $teacher;
     }
 
-    public function AssignTeacher(string $teachercode):void{
-        $this->teachercode = $teachercode;
+    public function unassignTeacher(): void
+    {
+        $this->teacher = null;
     }
-    public function UnassignTeacher(): void{
-        $this->teachercode = null;
+
+    public function getTeacher(): ?Teacher
+    {
+        return $this->teacher;
+    }
+
+    public function getTeacherCode(): ?string
+    {
+        return $this->teacher?->Code();
     }
 
     public function toArray(): array
@@ -95,7 +126,7 @@ class Subject{
             'code_subject' => $this->code,
             'namesubject' => $this->namesubject,
             'duration' => $this->duration,
-            'teachercode' => $this->teachercode,
+            'code_teacher' => $this->getTeacherCode(),
             'course' => $this->course ? $this->course->toArray() : null
         ];
     }
